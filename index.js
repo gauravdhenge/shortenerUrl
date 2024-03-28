c5f3f0dac0466d68f5fc543f79fe55fb37614a5a
gauravdhenge <36477454+gauravdhenge@users.noreply.github.com>
2024-03-28 16:29:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { connectMongoDb } = require('./connection');
+
+const app = express();
+const PORT = 8001;
+const urlRouter = require("./routes/url");
+
+//connect mongodb
+connectMongoDb('mongodb://127.0.0.1:27017/myapp-2').then(() => console.log("Mongo DB Connected")).catch((err) => console.log(err));
+
+//adding url encodeing with middleware
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use("/url",urlRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server started: ${PORT}`);
+});

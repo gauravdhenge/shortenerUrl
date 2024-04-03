3ef20bf643281cf15e9500761a6f5206a8d187e6
gaurav dhenge <gmddhenge386@gmail.com>
2024-04-03 23:04:19 +0530

    add shortid redirect

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { connectMongoDb } = require('./connection');
-
+const  {urlData}  = require('./models/url')
 const app = express();
 const PORT = 8002;
 const urlRouter = require("./routes/url");
@@ -12,7 +12,20 @@ connectMongoDb('mongodb://127.0.0.1:27017/myapp-2').then(() => console.log("Mong
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use("/url",urlRouter)
+app.use("/url", urlRouter)
+
+app.get("/:shortid", async (req, res) => {
+    const shortid = req.params.shortid;
+    console.log("shortid:",shortid)
+
+    const entry = await urlData?.findOneAndUpdate({ shortid }, {
+        $push: {
+            visitHistory: { timestamps: Date.now(), }
+        },
+    });
+    console.log("entry:",entry)
+    res.redirect(entry.redirectURL);
+})
 
 app.listen(PORT, () => {
     console.log(`Server started: ${PORT}`);
